240b14b89f98f9ac7d7a06fb159c7bd8fc4e825c
“Patrick <pctennislessons@gmail.com>
2019-06-19 09:29:29 -0600

    cleaned up code ot make it more readable and added comments

--- lib/getdata.rb
+++ lib/getdata.rb
@@ -5,12 +5,23 @@ require_all "./lib"
 
 class Getdata
 
-    def self.get_rate(user_country) #user enters a country and this returns a rate.
-        url = "https://api.exchangeratesapi.io/latest?base=USD" #aip for up to date currency rates 
-        @@data = RestClient.get(url) 
-        @@response = JSON.parse(@@data) 
-        code = Country.all.select{|country| country[:countryName] == user_country}.first[:currencyCode] #gets the currencyCode when given country
-        @@response["rates"][code]
+    def self.retrieve_data
+        url = "https://api.exchangeratesapi.io/latest?base=USD" #Public aip for up to date currency rates 
+        @data = RestClient.get(url) 
+        @response = JSON.parse(@data) 
+    end
+
+    def self.get_rate(user_country) #user enters a country and an exchange rate is returned.
+        
+        self.retrieve_data #gets the api hash for USD base
+        
+        country_hash = Country.all.select do |country| #gets country hash when provided country
+            country[:countryName] == user_country
+        end 
+        
+        code = code.first[:currencyCode] #gets the currencyCode when given country_hash
+        
+        @response["rates"][code] #takes the API hash and returns the
     end
 
 
