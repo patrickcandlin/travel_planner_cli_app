2e58c3306cd783d28e4c2fe18a7327724910dad6
“Patrick <pctennislessons@gmail.com>
2019-06-18 18:41:00 -0600

    API is returning rates hash

--- lib/getdata.rb
+++ lib/getdata.rb
@@ -1,4 +1,16 @@
+require "pry"
+require 'rest-client'
+require "require_all"
+require_all "./lib"
+
 class Getdata
+
+    def self.get_rate #user enters a country and this returns a rate.
+        url = "https://api.exchangeratesapi.io/latest?base=USD" #aip for up to date currency rates 
+        @@data = RestClient.get(url) 
+        @@response = JSON.parse(@@data) 
+    end
+
 
 
 end
