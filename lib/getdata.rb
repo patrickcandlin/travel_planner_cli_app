cd39c9e028203601a316523401b34fb384a44f87
“Patrick <pctennislessons@gmail.com>
2019-06-18 19:14:47 -0600

    get_rate method with get the rate when passed a country

--- lib/getdata.rb
+++ lib/getdata.rb
@@ -5,10 +5,12 @@ require_all "./lib"
 
 class Getdata
 
-    def self.get_rate #user enters a country and this returns a rate.
+    def self.get_rate(user_country) #user enters a country and this returns a rate.
         url = "https://api.exchangeratesapi.io/latest?base=USD" #aip for up to date currency rates 
         @@data = RestClient.get(url) 
         @@response = JSON.parse(@@data) 
+        code = Country.all.select{|country| country[:countryName] == user_country}.first[:currencyCode] #gets the currencyCode when given country
+        @@response["rates"][code]
     end
 
 
