ce708749ed121853a843aaab7e5991d89f0454f7
“Patrick <pctennislessons@gmail.com>
2019-06-20 11:08:10 -0600

    writing class method

--- lib/models/country.rb
+++ lib/models/country.rb
@@ -1,4 +1,12 @@
 class Country < ActiveRecord::Base
     has_many :trips
     has_many :travelers, through: :trips
+
+    def real_country(country)
+        until Country.exists?(countryName: country) do
+          puts "Sorry I do not recognize that country. Please check your spelling."
+          country = gets.chomp
+        end
+        Country.find_by(countryName: country)
+    end
 end
