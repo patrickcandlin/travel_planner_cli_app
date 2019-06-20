74fc28579fe3142ac88280540d892e71e4e64234
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-06-19 20:53:06 -0600

    Implement CLI functionality

--- config/environment.rb
+++ config/environment.rb
@@ -7,11 +7,8 @@ ActiveRecord::Base.establish_connection(
     :adapter => "sqlite3",
     :database => "db/travelapp.sqlite"
 )
-<<<<<<< HEAD
-CommandLineInterface.main_menu
-# binding.pry
-=======
 
-# CommandLineInterface.say_hi
-binding.pry
->>>>>>> ee74087338622ac420c2634376d170aa8dfcaf52
+ActiveRecord::Base.logger = nil
+
+CommandLineInterface.runner
+# binding.pry
